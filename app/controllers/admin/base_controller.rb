d0a1571c4c116861b95d49c5bb0426578995cb9c
Zhenya Telyukov <telyukov@gmail.com>
2022-02-21 11:07:52 +0300

    migration to RoR 7 finished

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -8,21 +8,22 @@
 # 18.08.2017  replace 'before_filter' with 'before_action'
 # 27.10.2020  Ancestor Controller changed
 # 15.11.2020  No classic autoloader: Zeitwerk only!
+# 21.02.2022  Last update
 ################################################################################
 class Admin::BaseController < ActionController::Base
+  protect_from_forgery with: :exception
+
   include Zt    # Zeitwerk
   include Pundit
   include AdminAuthentication
-  # protect_from_forgery with: :exception
-  before_action :set_locale
+
   layout 'admin'
 
-  protect_from_forgery with: :exception
-
+  before_action :set_locale
   before_action :check_login
   before_action :current_user
   after_action  :verify_authorized, except: :index
-#  after_action  :verify_policy_scoped, only: :index
+# after_action  :verify_policy_scoped, only: :index
 
   # Globally rescue Authorization Errors in a controller
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized  # permission denied
